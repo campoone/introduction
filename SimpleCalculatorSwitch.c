b2db1230ebd86f4b2aee6f691dedf97ba3da68bb
Andy Campo <andycampo.r@gmail.com>
2022-08-01 16:15:41 -0500

    Advanced cal

--- SimpleCalculatorSwitch.c
+++ SimpleCalculatorSwitch.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
+int main(){
+    char ch;
+    int a, b, result;
 
-    char op;
-    double first, second;
-    printf("Enter an operation (+, -, *, /): ");
-    scanf("%c", &op);
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &first, &second);
+    // Asking for Input
+    printf("Enter an Operator (+, -, *, /, !, s, p)! If you are doing ! or s, please enter the same number. \n If you are doing p, the first number is the base and the second number is the power. ");
+    scanf("%c", &ch);
+    printf("Enter two operands: \n");
+    scanf("%d %d", &a, &b);
+    int fact = 1;
+    int sum = 0;
+    int pow = 1;
 
-    switch (op) {
+    switch(ch){
         case '+':
-            printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+            result = a + b;
+            printf("Result = %d", result);
             break;
         case '-':
-            printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+            result = a - b;
+            printf("Result = %d", result);
             break;
         case '*':
-            printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+            result = a * b;
+            printf("Result = %d", result);
             break;
         case '/':
-            printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+            result = a / b;
+            printf("Result = %d", result);
             break;
-        default:
-            printf("The operator you entered is not valid. Please enter a valid operater.");
+        case '!':
+            while (a>0)
+            {
+                fact = fact * a;
+                a--;
+            }
+            printf("Factorial is %d\n",fact);
+            break;
+        case 's':
+            while (a>0)
+            {
+                sum = sum+(a%10);
+                a = a/10;
+            }
+            printf("Result = %d\n",sum);
+            break;
+        case 'p':
+            while (b>0)
+            {
+                pow = pow * a;
+                b--;
+            }
+            printf("Result = %d",pow);
+            break;
     }
     return 0;
 }
